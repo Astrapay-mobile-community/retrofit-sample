3c8aadc97ac952a205dacde7cbf3bd6fd83d02bd
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2023-03-15 09:14:12 +0700

    Fix typo

--- app/src/main/java/com/example/networkingsample/TodosApi.kt
+++ app/src/main/java/com/example/networkingsample/TodosApi.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface TodosApi {
-    @GET("/todossa?_sort=id&_order=desc&_limit=10")
+    @GET("/todos?_sort=id&_order=desc&_limit=10")
     suspend fun getTodos(): Response<Array<TodosResponse>>
 }
